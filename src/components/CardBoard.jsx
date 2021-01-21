ecd5cf7ce7e66f66b2d365caf9bfbe350ce96427
jameskip <me@jameskip.com>
2021-01-20 23:23:39 -0600

    Add new 117th Congress

--- src/components/CardBoard.jsx
+++ src/components/CardBoard.jsx
@@ -5,22 +5,24 @@ import SenateList from './SenateList'
 const CardBoard = props => {
   const [senate, setSenate] = useState()
   const [house, sethouse] = useState()
+  const houseUrl = 'https://api.propublica.org/congress/v1/117/house/members.json'
+  const senateUrl = 'https://api.propublica.org/congress/v1/117/senate/members.json'
   const options = { headers: { 'X-API-Key': 'JSp1AQhdSIuQQssE07bf5bsDT7HTpPDVQLAda1nx' }, mode: 'cors' }
 
   useEffect(() => {
     if (!house) {
-      fetch('https://api.propublica.org/congress/v1/116/house/members.json', options) // eslint-disable-line
+      fetch(houseUrl, options) // eslint-disable-line
         .then(house => house.json())
         .then(houseJson => sethouse({ house: houseJson.results[0].members }))
         .catch(error => console.error(error))
     }
     if (!senate) {
-      fetch('https://api.propublica.org/congress/v1/116/senate/members.json', options) // eslint-disable-line
+      fetch(senateUrl, options) // eslint-disable-line
         .then(senate => senate.json())
         .then(senateJson => setSenate({ senate: senateJson.results[0].members }))
         .catch(error => console.error(error))
     }
-  }, [senate, house])
+  })
 
   return (
       <>
